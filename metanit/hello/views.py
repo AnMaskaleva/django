8c47c93ee0ddd72bc7f2a16c0500f40ca23ab5b9
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-10 02:52:22 +0300

    redirect, permanent_redir

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,11 +1,13 @@
-from tkinter.font import names
-
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+    return HttpResponse("Index")
 
-def user(request):
-    age = request.GET.get("age", 0)
-    name = request.GET.get("name", "Undefined")
-    return HttpResponse(f"<h2>Имя: {name} Возраст: {age}</h2>")
+def about(request):
+    return HttpResponse("About")
+
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
