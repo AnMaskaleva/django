a609e03a942bbe0c97dbcacc0d250ce00874f8ea
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-10 02:42:39 +0300

    pflhfk ubn

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -6,6 +6,6 @@ def index(request):
     return HttpResponse("<h2>Главная</h2>")
 
 def user(request):
-    age = request.GET.get("age")
-    name = request.GET.get("name")
+    age = request.GET.get("age", 0)
+    name = request.GET.get("name", "Undefined")
     return HttpResponse(f"<h2>Имя: {name} Возраст: {age}</h2>")
