54c42f98c4ebf0fb713a383cdcc741efb5eae7ea
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-10 03:52:28 +0300

    куки

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,17 +1,7 @@
-from django.http import JsonResponse
-from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse
 
-def index(requst):
-    bob = Person("Bob", 41)
-    return JsonResponse(bob, safe=False, encoder=PersonEncorder)
-
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-class PersonEncorder(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Person):
-            return {"name": obj.name, "age": obj.age}
-        return super().default(obj)
+def set(request):
+    username = request.GET.get("username", "Undefined")
+    response = HttpResponse(f"Hello {username}")
+    response.set_cookie("username", username)
+    return response
