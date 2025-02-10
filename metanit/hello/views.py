68a7a810793e7c72091b614b7a5b15f36ebe6bae
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-10 03:30:23 +0300

    jsonresponse

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,16 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
+from django.http import JsonResponse
 
-def index(request, id):
-    people = ["Tom", "Bob", "Sam"]
-    if id in range(0, len(people)):
-        return HttpResponse(people[id])
-    else:
-        return HttpResponseNotFound("Not Found")
-
-def access(request, age):
-    if age not in range(1, 111):
-        return HttpResponseBadRequest("Некорректные данные")
-    if (age > 17):
-        return HttpResponse("Доступ разрешен")
-    else:
-        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+def index(requst):
+    return JsonResponse({"name":"Tom", "age":"38"})
