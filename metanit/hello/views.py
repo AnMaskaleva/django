d6e231ca5ea7cf758ea876095e5885e0f33b65bd
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-10 03:40:13 +0300

    DjangoJSONEncoder

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,4 +1,17 @@
 from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
 
 def index(requst):
-    return JsonResponse({"name":"Tom", "age":"38"})
+    bob = Person("Bob", 41)
+    return JsonResponse(bob, safe=False, encoder=PersonEncorder)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class PersonEncorder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Person):
+            return {"name": obj.name, "age": obj.age}
+        return super().default(obj)
