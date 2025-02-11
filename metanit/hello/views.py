49786eb289a0828bb9959c98ce7fa66b6ecbdb12
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-11 03:53:52 +0300

    шаблоны, добавление стр

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def set(request):
-    username = request.GET.get("username", "Undefined")
-    response = HttpResponse(f"Hello {username}")
-    response.set_cookie("username", username)
-    return response
+def index(request):
+    return render(request, "index.html")
 
-def get(request):
-    username = request.COOKIES["username"]
-    return HttpResponse(f"Hello {username}")
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
