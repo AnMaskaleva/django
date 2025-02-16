12b41e206a684067eb6e6bf91c4c90cf5b222fde
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-16 23:21:35 +0300

    статистические файлы

--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
