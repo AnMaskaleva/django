59c6e2c9d81950ebb38087e83887e7952ed9ee7f
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-02-16 23:43:53 +0300

    TemplateView передача данных в шаблон

--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("about/", TemplateView.as_view(template_name="about.html")),
+    path("about/", TemplateView.as_view(template_name="about.html",
+        extra_context={"header":"О сайте"})),
     path("contact/", TemplateView.as_view(template_name="contact.html")),
 ]
